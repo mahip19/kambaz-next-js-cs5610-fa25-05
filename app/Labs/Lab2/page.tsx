f328d03256415879d283cd8bbe92d5f3890f0027
Mahip <mahipparekh1920@gmail.com>
2025-09-30 11:07:12 -0400

    removed epos

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -29,7 +29,7 @@ export default function Lab2() {
       <h3>Styling with the STYLE attribute</h3>
       <p>
         Style attribute allows configuring look and feel right on the element.
-        Although it's very convenient it is considered bad practice and you
+        Although it is very convenient it is considered bad practice and you
         should avoid using the style attribute
       </p>
 
@@ -40,8 +40,8 @@ export default function Lab2() {
           name, e.g., P, we can refer to a specific element by its ID
         </p>
         <p id="wd-id-selector-2">
-          Here's another paragraph using a different ID and a different look and
-          feel
+          Here is another paragraph using a different ID and a different look
+          and feel
         </p>
       </div>
 
@@ -49,7 +49,7 @@ export default function Lab2() {
         <h3>Class selectors</h3>
 
         <p className="wd-class-selector">
-          Instead of using IDs to refer to elements, you can use an element's
+          Instead of using IDs to refer to elements, you can use an elements
           CLASS attribute
         </p>
 
@@ -65,7 +65,7 @@ export default function Lab2() {
             Selectors can be combined to refer elements in particular places in
             the document
             <p className="wd-selector-3">
-              This paragraph's red background is referenced as
+              This paragraph red background is referenced as
               <br />
               .selector-2 .selector3
               <br />
