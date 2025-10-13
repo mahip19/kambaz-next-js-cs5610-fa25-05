0301aaef4d7f9e44610c968201b371d0c2c71bef
Mahip <mahipparekh1920@gmail.com>
2025-10-13 16:47:03 -0400

    added key in simplearrys'

--- app/Labs/Lab3/SimpleArrays.tsx
+++ app/Labs/Lab3/SimpleArrays.tsx
@@ -4,7 +4,10 @@ export default function SimpleArrays() {
   const constant1 = functionScoped - blockScoped;
   const numberArray1 = [1, 2, 3, 4, 5];
   const stringArray1 = ["string1", "string2"];
-  const htmlArray1 = [<li>Buy milk</li>, <li>Feed the pets</li>];
+  const htmlArray1 = [
+    <li key={1}>Buy milk</li>,
+    <li key={2}>Feed the pets</li>,
+  ];
   const variableArray1 = [
     functionScoped,
     blockScoped,
