f2da54c8a1a54b82993ba97ba9e6cafc61d59ce6
Mahip <mahipparekh1920@gmail.com>
2025-10-13 18:43:03 -0400

    double check rubrics before wednesday

--- app/(Kambaz)/Courses/[cid]/Breadcrumb.tsx
+++ app/(Kambaz)/Courses/[cid]/Breadcrumb.tsx
@@ -8,9 +8,26 @@ export default function Breadcrumb({
   course: { name: string } | undefined;
 }) {
   const pathname = usePathname();
-  return (
-    <span>
-      {course?.name} &gt; {pathname.split("/").pop()}
-    </span>
+
+  const segments = pathname.split("/").filter(Boolean);
+  // ["Courses", "RS101", "People", "Table"]
+  const afterCourse = segments.slice(2);
+
+  let formattedSegments = [...afterCourse];
+
+  //  /People/Table, collapse to just People
+  if (
+    formattedSegments.length === 2 &&
+    formattedSegments[0] === "People" &&
+    formattedSegments[1] === "Table"
+  ) {
+    formattedSegments = ["People"];
+  }
+  formattedSegments = formattedSegments.map((seg) =>
+    seg === "Table" ? "People" : seg
   );
+
+  const breadcrumbTrail = [course?.name, ...formattedSegments].join(" > ");
+
+  return <span>{breadcrumbTrail}</span>;
 }
