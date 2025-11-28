1140abac676e661efe9a2c0258778d4bdd105aa5
Mahip <mahipparekh1920@gmail.com>
2025-11-27 19:52:40 -0500

    a5

--- app/Labs/Lab5/PathParameters.tsx
+++ app/Labs/Lab5/PathParameters.tsx
@@ -36,6 +36,20 @@ export default function PathParameters() {
       >
         Substract {a} - {b}
       </a>
+      <a
+        className="btn btn-warning me-2"
+        id="wd-path-parameter-multiply"
+        href={`${HTTP_SERVER}/lab5/multiply/${a}/${b}`}
+      >
+        Multiply {a} × {b}
+      </a>
+      <a
+        className="btn btn-info"
+        id="wd-path-parameter-divide"
+        href={`${HTTP_SERVER}/lab5/divide/${a}/${b}`}
+      >
+        Divide {a} ÷ {b}
+      </a>
       <hr />
     </div>
   );
